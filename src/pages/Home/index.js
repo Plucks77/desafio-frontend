0791e79e25c589d28b265ad00bb1c8bb93a3b2a6
Pedro Lucas <pedro_leoti@hotmail.com>
2021-03-04 19:55:03 -0300

    Update - Retorno do id da categoria no método de selecionar todas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import api from "../../services/api";
 import {
   Container,
   Title,
@@ -16,6 +17,23 @@ import {
 import Nav from "../../components/Navbar";
 
 function Home() {
+  const [products, setProducts] = useState(null);
+  const [categories, setCategories] = useState(null);
+
+  async function loadProducts() {
+    const response = await api.get("/products");
+    setProducts(response.data);
+  }
+
+  async function loadCategories() {
+    const response = await api.get("/categories");
+    setCategories(response.data);
+  }
+  useEffect(() => {
+    loadProducts();
+    loadCategories();
+  }, []);
+
   return (
     <>
       <Nav />
@@ -25,8 +43,12 @@ function Home() {
         <CategoryProductContainer>
           <CategoryContainer>
             <Category>Categorias:</Category>
-            <Category>Bebidas (5)</Category>
-            <Category>Doces (3)</Category>
+            {categories &&
+              categories.map((category) => (
+                <Category key={category.id}>
+                  {category.name} ({category.amount})
+                </Category>
+              ))}
           </CategoryContainer>
 
           <ProductContainer>
@@ -42,8 +64,18 @@ function Home() {
               </ProductData>
               <AddToCart>Adicionar ao carrinho</AddToCart>
             </Product>
-
-            <Product>
+            {products &&
+              products.map((product) => (
+                <Product key={product.id}>
+                  <ProductImage src={product.image_url} />
+                  <ProductData>
+                    <ProductText>{product.name}</ProductText>
+                    <ProductText>R$ {product.price}</ProductText>
+                  </ProductData>
+                  <AddToCart>Adicionar ao carrinho</AddToCart>
+                </Product>
+              ))}
+            {/* <Product>
               <ProductImage
                 src={
                   "https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG"
@@ -119,7 +151,7 @@ function Home() {
                 <ProductText>R$ 10,00</ProductText>
               </ProductData>
               <AddToCart>Adicionar ao carrinho</AddToCart>
-            </Product>
+            </Product> */}
           </ProductContainer>
         </CategoryProductContainer>
       </Container>
