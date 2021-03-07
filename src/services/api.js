5352643fa8e8508d27a517c5f8402dda40ed3fc4
Pedro Lucas <pedro_leoti@hotmail.com>
2021-03-07 20:12:06 -0300

    Update - Atualização do backend para o heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const local = "http://localhost:3333";
-const production = "http://3.138.161.15";
+const production = "https://desafio-tecnico-backend7.herokuapp.com";
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === "production" ? production : local,
