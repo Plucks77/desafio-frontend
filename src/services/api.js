2fe15665120631752a071de77fd55080bd32be96
Pedro Lucas <pedro_leoti@hotmail.com>
2021-03-07 18:44:57 -0300

    Update - Adicionada a url de produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const local = "http://localhost:3333";
+const production = "http://3.138.161.15";
 
 const api = axios.create({
-  baseURL: local,
+  baseURL: process.env.NODE_ENV === "production" ? production : local,
 });
 
 export default api;
